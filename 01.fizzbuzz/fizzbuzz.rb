5d8b43958da1d2135ee24a08d1ad845b57aafab9
ryu-tkda <ssmddikbb.g@gmail.com>
2021-06-27 23:08:08 +0900

    rubocop処理

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 (1..20).each do |i|
-  if (i % 3 == 0) && (i % 5 == 0)
+  if (i % 3).zero? && (i % 5).zero?
     puts 'FizzBuzz'
-  elsif i % 3 == 0
+  elsif (i % 3).zero?
     puts 'Fizz'
-  elsif i % 5 == 0
+  elsif (i % 5).zero?
     puts 'Buzz'
   else
     puts i
