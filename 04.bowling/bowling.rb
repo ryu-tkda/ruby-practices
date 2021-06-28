18e545485474ec419509d0c3ee1734978ebefac4
ryu-tkda <ssmddikbb.g@gmail.com>
2021-06-28 14:45:27 +0900

    スコア計算リファクタリング

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,37 +1,28 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# 引数を取得
-# (,)区切りで配列に展開
 score = ARGV[0]
 scores = score.split(',')
 
 # 文字列を整数に
 shots = []
-framebox = 0
 scores.each do |s|
-  if framebox >= 18
+  if shots.size >= 18
     if s == 'X'
       shots << 10
-      framebox += 1
       next
     end
   elsif s == 'X'
     shots << 10
     shots << 0
-    framebox += 2
     next
   end
   shots << s.to_i
-  framebox += 1
 end
 
 # フレームごとに分ける
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
-unless (frames[10]).nil?
+frames = shots.each_slice(2).to_a
+if frames[10]
   frames[9].push(*frames[10])
   frames.pop(1)
 end
@@ -39,31 +30,25 @@ end
 # スコアの計算
 point = 0
 
-frames.each_with_index do |_frame, i|
-  point += if frames[i][0] == 10 && i != 9 && i + 1 != 9
-             if frames[i + 1][0] == 10 && i + 2 != 9
-               20 + frames[i + 2][0]
-             elsif frames[i + 2][0] == 10 && i + 2 == 9
-               30
-             elsif frames[i + 2][0] != 10 && i + 2 == 9
-               20 + frames[i + 2][0]
+frames.each_with_index do |frame, i|
+  point += if i < 8
+             if frame[0] == 10
+               frames[i + 1][0] == 10 ? 20 + frames[i + 2][0] : 10 + (frames[i + 1]).sum
+             elsif frame.sum == 10
+               10 + frames[i + 1][0]
              else
-               10 + frames[i + 1][0] + frames[i + 1][1]
+               frame.sum
              end
-           elsif frames[i][0] == 10 && i != 9 && i + 1 == 9
-             10 + frames[i + 1][0] + frames[i + 1][1]
-           elsif frames[i][0] != 10 && frames[i][0] + frames[i][1] == 10 && i != 9
-             10 + frames[i + 1][0]
-           elsif i == 9
-             if frames[i][0] == 10
-               frames[i][1] == 10 ? 20 + frames[i][2] : 10 + frames[i][1]
-             elsif frames[i][0] != 10 && frames[i][0] + frames[i][1] == 10
-               10 + frames[i][2]
+           elsif i == 8
+             if frame[0] == 10
+               frames[i + 1][0] == 10 ? 20 + frames[i + 1][1] : 10 + frames[i + 1][0] + frames[i + 1][1]
+             elsif frame.sum == 10
+               10 + frames[i + 1][0]
              else
-               frames[i][0] + frames[i][1]
+               frame.sum
              end
            else
-             frames[i][0] + frames[i][1]
+             frame.sum
            end
 end
 puts point
