35257727e5e9e99deb19b8f4c6f10aa83e81b91e
ryu-tkda <ssmddikbb.g@gmail.com>
2021-06-28 21:54:28 +0900

    ９フレーム目の処理変更

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -7,17 +7,12 @@ scores = score.split(',')
 # 文字列を整数に
 shots = []
 scores.each do |s|
-  if shots.size >= 18
-    if s == 'X'
-      shots << 10
-      next
-    end
-  elsif s == 'X'
+  if s == 'X'
     shots << 10
-    shots << 0
-    next
+    shots << 0 if shots.size < 18
+  else
+    shots << s.to_i
   end
-  shots << s.to_i
 end
 
 # フレームごとに分ける
@@ -31,22 +26,12 @@ end
 point = 0
 
 frames.each_with_index do |frame, i|
-  point += if i < 8
-             if frame[0] == 10
-               frames[i + 1][0] == 10 ? 20 + frames[i + 2][0] : 10 + (frames[i + 1]).sum
-             elsif frame.sum == 10
-               10 + frames[i + 1][0]
-             else
-               frame.sum
-             end
-           elsif i == 8
-             if frame[0] == 10
-               frames[i + 1][0] == 10 ? 20 + frames[i + 1][1] : 10 + frames[i + 1][0] + frames[i + 1][1]
-             elsif frame.sum == 10
-               10 + frames[i + 1][0]
-             else
-               frame.sum
-             end
+  point += if i < 8 && frame[0] == 10
+             frames[i + 1][0] == 10 ? 20 + frames[i + 2][0] : 10 + (frames[i + 1]).sum
+           elsif i == 8 && frame[0] == 10
+             frames[i + 1][0] == 10 ? 20 + frames[i + 1][1] : 10 + frames[i + 1][0] + frames[i + 1][1]
+           elsif i != 9 && frame.sum == 10
+             10 + frames[i + 1][0]
            else
              frame.sum
            end
