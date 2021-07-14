5cbd1d01f5f5ca0d9c567088d6bbb47a3cd0a456
ryu-tkda <ssmddikbb.g@gmail.com>
2021-07-14 12:21:03 +0900

    ハッシュに変更

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -5,89 +5,39 @@ require 'optparse'
 require 'etc'
 
 def file_type(type)
-  case type
-  when 'fifo'
-    print 'p'
-  when 'characterSpecial'
-    print 'c'
-  when 'directory'
-    print 'd'
-  when 'blockSpecial'
-    print 'b'
-  when 'file'
-    print '-'
-  when 'link'
-    print 'l'
-  when 'socket'
-    print 's'
-  end
+  types = { "fifo": 'p', "characterSpecial": 'c', "directory": 'd', "blockSpecial": 'b', "file": '-', "link": 'l', "socket": 's' }
+  types[type.to_sym]
 end
 
-def user_group_name
+def to_display_user_group_name
   user_id    = Process.uid
   user_name  = Etc.getpwuid(user_id).name
   group_id   = Process.gid
   group_name = Etc.getgrgid(group_id).name
-  print '  '
-  print user_name
-  print '  '
-  print group_name
-  print '  '
+  print "  #{user_name}  #{group_name}  "
 end
+
 def file_permission(permission)
-  case permission
-  when '0'
-    print '---'
-  when '1'
-    print '--x'
-  when '2'
-    print '-w-'
-  when '3'
-    print '-wx'
-  when '4'
-    print 'r--'
-  when '5'
-    print 'r-x'
-  when '6'
-    print 'rw-'
-  when '7'
-    print 'rwx'
-  end
+  permissions = { "0": '---', "1": '--x', "2": '-w-', "3": '-wx', "4": 'r--', "5": 'r-x', "6": 'rw-', "7": 'rwx' }
+  permissions[permission.to_sym]
 end
 
 opt = OptionParser.new
 params = opt.getopts(ARGV, 'alr')
 files = params['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
 files = files.reverse if params['r']
-max_size = files.map(&:size).max
-column = 3
-fsize = files.size
-lines = fsize / column
-trust = fsize % column
-lines += 1 if trust != 0 && lines > 1
-line = files.each_slice(lines).to_a
-i = 0
-if lines <= 1
-  until i == trust
-    line[i].push(*line[i + 1])
-    line.delete_at(i + 1)
-    i += 1
-  end
-end
-line.map! { |a| a.values_at(0..lines) }
-results = line.transpose
 
 if params['l']
   files.each do |fl|
     files_info = File.lstat(fl)
-    file_type(files_info.ftype)
+    print file_type(files_info.ftype)
     permissions = files_info.mode.to_s(8).split(//).pop(3)
     permissions.each do |permission|
-      file_permission(permission)
+      print file_permission(permission)
     end
     print '  '
     print files_info.nlink
-    user_group_name
+    to_display_user_group_name
     print files_info.size
     print '  '
     print files_info.mtime.month
@@ -104,7 +54,16 @@ if params['l']
     print "\n"
   end
 else
+  max_size = files.map(&:size).max
+  column = 3
+  fsize = files.size
+  lines = fsize / column
+  trust = fsize % column
+  lines += 1 if trust != 0
+  line = files.each_slice(lines).to_a
+  line.map! { |a| a.values_at(0..lines) }
+  results = line.transpose
   results.map do |items|
-    puts items.map { |item| "%-#{max_size}s  " % item }.join
+    puts items.map { |item| format("%-#{max_size}s  ", item) }.join
   end
 end
